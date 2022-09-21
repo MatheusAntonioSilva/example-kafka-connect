28cc556623013d8d99e4d2290948a93267867c21
Matheus Antonio Silva <matheus-silva@matheus.silva>
2022-09-20 22:41:16 -0300

    Fix: Exemplo Kafka-connect integração duas bases

--- sql_for_tests.sql
+++ sql_for_tests.sql
@@ -1,14 +1,12 @@
 use fullcycle;
-CREATE TABLE products (id int, description varchar(255));
-INSERT INTO fullcycle.products (id, description) VALUES (1, 'Test');
+CREATE TABLE products (id int, description varchar(255), created_at timestamp);
+INSERT INTO fullcycle.products (id, description, created_at) VALUES (1, 'Test', '2022-05-09 00:00:01');
 
 CREATE DATABASE tests_connect;
 USE tests_connect;
-CREATE TABLE new_products (id int, description varchar(255));
+CREATE TABLE new_products (id int, description varchar(255), created_at timestamp);
 
-USE fullcycle;
-INSERT INTO fullcycle.products (id, description) VALUES (2, 'Test novo');
-USE tests_connect;
+INSERT INTO fullcycle.products (id, description), created_at VALUES (2, 'Test novo', '2022-05-10 00:00:01');
 SELECT * FROM tests_connect.new_products;
 
-INSERT INTO fullcycle.products (id, description) VALUES (2, 'Test novo');
+INSERT INTO fullcycle.products (id, description, created_at) VALUES (3, 'Test wwwww', '2022-05-10 00:00:02');
